7f03792179c25a27952d46afe3e8f40708eb2486
Thomas Pflüger <mr.plow@posteo.de>
2026-04-15 20:57:04 +0200

    lspconfig cleanup

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,51 +1,34 @@
--- EXAMPLE 
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local on_init = require("nvchad.configs.lspconfig").on_init
-local capabilities = require("nvchad.configs.lspconfig").capabilities
+require("nvchad.configs.lspconfig").defaults()
+local servers = {
+    html = {},
+    cssls = {},
+    clangd = {
+        cmd = {
+            "clangd",
+            "--background-index",
+            "--completion-style=detailed",
+            "--enable-config",
+            "--function-arg-placeholders=false",
+            "--all-scopes-completion",
+            "--header-insertion-decorators",
+            "--header-insertion=never",
+            "--log=verbose",
+            "--pch-storage=memory",
+            "--pretty",
+            "--suggest-missing-includes",
+        },
+    },
+    gopls = {},
+    pylsp = {
+        settings = {
+            autoimport = true,
+        },
+    },
+}
 
-local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd", "gopls", "pylsp" }
-
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-  }
+for name, opts in pairs(servers) do
+    vim.lsp.config(name, opts)
+    vim.lsp.enable(name)
 end
 
--- typescript
--- lspconfig.tsserver.setup {
---   on_attach = on_attach,
---   on_init = on_init,
---   capabilities = capabilities,
--- }
-lspconfig["pylsp"].setup {
-    settings = {
-        autoimport = true,
-    },
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-}
-
-lspconfig["clangd"].setup {
-    cmd = {
-        "clangd",
-        "--background-index",
-        "--completion-style=detailed",
-        "--enable-config",
-        "--function-arg-placeholders=false",
-        "--all-scopes-completion",
-        "--header-insertion-decorators",
-        "--header-insertion=never",
-        "--log=verbose",
-        "--pch-storage=memory",
-        "--pretty",
-        "--suggest-missing-includes",
-    },
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-}
+vim.lsp.enable(servers)
