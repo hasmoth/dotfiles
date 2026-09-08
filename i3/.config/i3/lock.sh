12b6f76967f0cb327aa6a93f2a6182daab74a941
Thomas Pflüger <mr.plow@posteo.de>
2026-09-08 21:44:59 +0200

    several changes for lock screen, layout switch and new picom module

--- i3/.config/i3/lock.sh
+++ i3/.config/i3/lock.sh
@@ -3,4 +3,18 @@ tmpbg="/tmp/tmpbg.png"
 scrot -o "$tmpbg"
 convert "$tmpbg" -blur 4x8 "$tmpbg"
 setxkbmap us
-i3lock -i "$tmpbg"
+
+
+# i3lock starten, OHNE den Sleep-Lock-FD zu vererben
+if [ -n "${XSS_SLEEP_LOCK_FD}" ] && [ -e "/dev/fd/${XSS_SLEEP_LOCK_FD}" ]; then
+    i3lock --nofork --tiling --image="$tmpbg" {XSS_SLEEP_LOCK_FD}<&-
+    # FD jetzt schließen → System darf suspendieren
+    exec {XSS_SLEEP_LOCK_FD}<&-
+else
+    i3lock --nofork --tiling --image="$tmpbg"
+fi
+
+tail --pid="$(pidof -s i3lock)" -f /dev/null
+
+
+# i3lock -i "$tmpbg"
